50ff2bd79029df85761a4dabbdc26db96ab8154e
rfkih <rifkihidayat23@yahoo.com>
2022-07-03 21:29:33 +0700

    add product

--- src/routers/products/post.products.js
+++ src/routers/products/post.products.js
@@ -5,6 +5,36 @@ const upload = require("../../services/upload");
 
 
 
+//Post Product Router
+const postProductRouter = async (req, res, next) => {
+
+  const connection = await pool.promise().getConnection();
+  
+  try {
+    const sqlPostProduct = "INSERT INTO products SET ?";
+
+    const dataProduct = [
+      {
+        category_id: req.body.newProduct.category_id,
+        productName: req.body.newProduct.productName,
+        productDetails: req.body.newProduct.productDetails,
+        productIMG: req.body.newProduct.productIMG,
+        isLiquid: req.body.newProduct.isLiquid,
+        isDeleted: req.body.newProduct.isDeleted,
+        price: req.body.newProduct.price,
+      },
+    ];
+
+    const [result] = await connection.query(sqlPostProduct, dataProduct)
+
+    res.send("Input Product success");
+    connection.release();
+  } catch (error) {
+    connection.release();
+    next(error);
+  }
+}
+
 
 const multerUploadSingle = upload.uploadProductPhoto.single("productPhoto");
 
@@ -20,7 +50,7 @@ const postProductPhotoRouter = async (req, res) => {
   res.json({ status: "success", image: finalImageURL });
 };
 
-
+router.post("/", postProductRouter);
 router.post("/upload", multerUploadSingle, postProductPhotoRouter);
 
 module.exports = router;
